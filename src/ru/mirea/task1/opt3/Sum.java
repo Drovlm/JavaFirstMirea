fe3d53cc75cea7b9931c47bc0a157ebdbbf6fba5
Mudar <modarking.mj@gmail.com>
2021-10-06 21:59:37 +0300

    added class Sum.java

---
+++ src/ru/mirea/task1/opt3/Sum.java
@@ -0,0 +1,12 @@
+package ru.mirea.task1.opt3;
+
+public class Sum {
+
+    public static void main(String[] args)
+    {
+        int a = 62;
+        int b = 18;
+        // It will return the sum of two arguments.
+        System.out.println("The sum is =" + Integer.sum(a, b));
+    }
+}
