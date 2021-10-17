5ebf5f64dcc644cdbc501441a3be43078523b028
Mudar <modarking.mj@gmail.com>
2021-10-17 14:58:41 +0300

    Added class GetName.java

---
+++ src/ru/mirea/task6/GetName.java
@@ -0,0 +1,11 @@
+package ru.mirea.task6;
+
+public class GetName {
+    String name;
+    
+  public static void main (String[] args){
+    GetName a = new GetName();
+   a.name = "Mudar";
+
+      System.out.println("Hi " +a.name);
+}}
