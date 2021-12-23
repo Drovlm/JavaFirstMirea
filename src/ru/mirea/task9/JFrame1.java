b7eb36fba0e666459a68c038a75d2d57a1f1d8d8
Mudar <modarking.mj@gmail.com>
2021-12-23 18:31:14 +0300

    Added class GameT.java

--- src/ru/mirea/task9/JFrame1.java
+++ src/ru/mirea/task9/JFrame1.java
@@ -3,6 +3,7 @@ package ru.mirea.task9;
 import javax.swing.*;
 
 public class JFrame1 {
+
     private JButton Madrid;
     private JPanel Panel1;
     private JButton Milan;
@@ -16,7 +17,7 @@ public class JFrame1 {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("JFrame");
-        frame.setContentPane(new JFrame1(). Panel1);
+        frame.setContentPane(new JFrame1().Panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
